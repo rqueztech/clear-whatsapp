98f28ab8a2bbff503a4154f9b524049b92dcf3d7
= <=>
2026-04-17 19:52:46 +0900

    added toggle feature

--- content.js
+++ content.js
@@ -1,11 +1,9 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "BUTTON_CLICKED") {
-    console.log("Button clicked in extension!");
-
-    const event = new CustomEvent("my-extension-event", {
-      detail: { message: "Hello from extension" }
-    });
-
-    window.dispatchEvent(event);
+        const el = document.querySelector('#app > div > div > div.x78zum5.xdt5ytf.x5yr21d > div > div._aigw._as6h.x9f619.x1n2onr6.x5yr21d.x17dzmu4.x1i1dayz.x2ipvbc.xjdofhw.x78zum5.xdt5ytf.x12xzxwr.x1plvlek.xryxfnj.x570efc.x18dvir5.xxljpkc.xwfak60.x18pi947');
+        
+      if (el) {
+	  el.style.display = (el.style.display === 'none') ? '' : 'none';
+      }
   }
 });
